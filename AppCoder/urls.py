fd7c4211b81ca4a4c4706efb5212ca8c8b5de776
Luis Carlos Herrera <luis.herrera@mercadolibre.com.co>
2023-03-13 19:06:42 -0500

    creando forms

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,6 +4,7 @@ from AppCoder.views import *
 
 urlpatterns = [
     path('cursos', cursos, name="AppCoderCursos"),
+    path('buscar_curso', busqueda_curso, name="AppCoderBuscarCurso"),
     path('curso/<nombre>/<camada>', crear_curso, name="AppCoderCurso"),
     path('estudiantes', estudiantes, name="AppCoderEstudiantes"),
     path('profesores', profesores, name="AppCoderProfesores"),
