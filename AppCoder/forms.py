7e971750e0b42755c8d18756c9ab5970303cda62
Luis Carlos Herrera <luis.herrera@mercadolibre.com.co>
2023-03-15 18:54:19 -0500

    creacion del crud para cursos

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -1,10 +1,27 @@
 from django import forms
 
+from AppCoder.models import Curso, Estudiantes, Profesor
 
-class CursoForm(forms.Form):
 
-    nombre = forms.CharField(min_length=3, max_length=40)
-    camada = forms.IntegerField(min_value=1000)
+class CursoForm(forms.ModelForm):
+
+    class Meta:
+        model = Curso
+        fields = "__all__"
+
+
+class EstudianteForm(forms.ModelForm):
+
+    class Meta:
+        model = Estudiantes
+        fields = "__all__"
+
+
+class ProfesorForm(forms.ModelForm):
+
+    class Meta:
+        model = Profesor
+        fields = "__all__"
 
 
 class BusquedaCursoForm(forms.Form):
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -16,8 +16,33 @@ def busqueda_curso(request):
         return render(request, "AppCoder/busqueda_curso.html", context=context)
 
 
-def cursos(request):
+def editar_curso(request, camada):
+    get_curso = Curso.objects.get(camada=camada)
 
+    if request.method == "POST":
+        mi_formulario = CursoForm(request.POST)
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+
+            get_curso.nombre = informacion['nombre']
+            get_curso.camada = informacion['camada']
+
+            get_curso.save()
+            return redirect("AppCoderCursos")
+
+
+    context = {
+        "camada": camada,
+        "form": CursoForm(initial={
+            "nombre": get_curso.nombre,
+            "camada": get_curso.camada
+        })
+    }
+    return render(request, "AppCoder/editar_curso.html", context=context)
+
+
+def crear_curso(request):
     if request.method == "POST":
         mi_formulario = CursoForm(request.POST)
 
@@ -28,17 +53,31 @@ def cursos(request):
                 camada=informacion['camada']
             )
             curso_save.save()
+            return redirect("AppCoderCursos")
+
+    context = {
+        "form": CursoForm()
+    }
+    return render(request, "AppCoder/crear_curso.html", context=context)
+
+
+def elimiar_curso(request, camada):
+    get_curso = Curso.objects.get(camada=camada)
+    get_curso.delete()
+
+    return redirect("AppCoderCursos")
+
+def cursos(request):
 
     all_cursos = Curso.objects.all()
     context = {
         "cursos": all_cursos,
-        "form": CursoForm(),
         "form_busqueda": BusquedaCursoForm(),
     }
     return render(request, "AppCoder/cursos.html", context=context)
 
 
-def crear_curso(request, nombre, camada):
+def crear_curso1(request, nombre, camada):
     save_curso = Curso(nombre=nombre, camada=int(camada))
     save_curso.save()
     context = {
